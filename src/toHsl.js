87e61a2f0fe59c9a81ec4dbaecd9ded1362e1e82
David Wheatley <hi@davwheat.dev>
2021-02-18 16:36:17 +0000

    Fix hue output in ToHsl

--- src/toHsl.js
+++ src/toHsl.js
@@ -1,7 +1,7 @@
 export default function toHsl(color) {
   return (
     'hsla(' +
-    [color.h, color.s * 100 + '%', color.l * 100 + '%', color.a].join(',') +
+    [color.h * 360, color.s * 100 + '%', color.l * 100 + '%', color.a].join(',') +
     ')'
   )
 }
